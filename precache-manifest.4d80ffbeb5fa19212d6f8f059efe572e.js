aa16d52d7a725138aa782ef5f9271a2413f0d6eb
eunbi <silver23rain@gmail.com>
2020-02-22 05:22:56 +0900

    Updates

---
+++ precache-manifest.4d80ffbeb5fa19212d6f8f059efe572e.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f6f5fe23c48e94abd8d6027d93f1752",
+    "url": "/bookstore/index.html"
+  },
+  {
+    "revision": "83183664799b7bda5641",
+    "url": "/bookstore/static/css/main.378577ba.chunk.css"
+  },
+  {
+    "revision": "a7b1d36a5dd4dd25de0c",
+    "url": "/bookstore/static/js/2.bfa73fd2.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/bookstore/static/js/2.bfa73fd2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "83183664799b7bda5641",
+    "url": "/bookstore/static/js/main.e8184df6.chunk.js"
+  },
+  {
+    "revision": "09daa9c7c9875185a124",
+    "url": "/bookstore/static/js/runtime-main.515e3c15.js"
+  },
+  {
+    "revision": "ae16b2076022cac144ccce397a02aa2f",
+    "url": "/bookstore/static/media/210옴니고딕020.ae16b207.ttf"
+  },
+  {
+    "revision": "e0d684996117ad9e2c5bd7ba55f5dad2",
+    "url": "/bookstore/static/media/210옴니고딕030.e0d68499.ttf"
+  },
+  {
+    "revision": "845638d8b0ac340dca71b6ccc97b09a3",
+    "url": "/bookstore/static/media/210옴니고딕040.845638d8.ttf"
+  },
+  {
+    "revision": "1aa604877f02c4d2ccc2e71aa43a8a35",
+    "url": "/bookstore/static/media/210옴니고딕050.1aa60487.ttf"
+  },
+  {
+    "revision": "3a44ffb1539dbcfce07e73ce69ea55de",
+    "url": "/bookstore/static/media/AppleSDGothicNeoM.3a44ffb1.ttf"
+  },
+  {
+    "revision": "99b9c7ef2a5391b462dc0803eefdc8c3",
+    "url": "/bookstore/static/media/YellowCircle.99b9c7ef.png"
+  },
+  {
+    "revision": "b87d3e9a9e43ff190ee248b7d31c4832",
+    "url": "/bookstore/static/media/arrow.b87d3e9a.svg"
+  },
+  {
+    "revision": "4430dceb7530da059803783e0a29430c",
+    "url": "/bookstore/static/media/asset3.4430dceb.svg"
+  },
+  {
+    "revision": "959e8521d367a6af2a59ab91eef301c2",
+    "url": "/bookstore/static/media/book_sample.959e8521.png"
+  },
+  {
+    "revision": "20f166a2874b4a1d04de2f1f586a4f5f",
+    "url": "/bookstore/static/media/book_sample2.20f166a2.png"
+  },
+  {
+    "revision": "ec762faf55d0f1d0b8d5205b641f9e39",
+    "url": "/bookstore/static/media/book_sample3.ec762faf.png"
+  },
+  {
+    "revision": "2f282f231a2d15d31187870ae21ea751",
+    "url": "/bookstore/static/media/cassette.2f282f23.png"
+  },
+  {
+    "revision": "8d0aa95a990036272f24a573fa91a169",
+    "url": "/bookstore/static/media/circle_green.8d0aa95a.png"
+  },
+  {
+    "revision": "9bcfcee2c14dae70eb99ecf70ff17a3a",
+    "url": "/bookstore/static/media/circle_red.9bcfcee2.png"
+  },
+  {
+    "revision": "518f2a626b01b88f87fd71ad1265b457",
+    "url": "/bookstore/static/media/circle_yellow.518f2a62.png"
+  },
+  {
+    "revision": "3e0609b7f7f3eacc53ab2328ce7d4b82",
+    "url": "/bookstore/static/media/dot_green.3e0609b7.png"
+  },
+  {
+    "revision": "7615929e37e948ba0d63cb4c16829993",
+    "url": "/bookstore/static/media/dot_red.7615929e.png"
+  },
+  {
+    "revision": "f682141f4c426edcf92398cffa73d2a3",
+    "url": "/bookstore/static/media/dot_yellow.f682141f.png"
+  },
+  {
+    "revision": "b88a7ce89a1ecb698573d9cdb3c4272a",
+    "url": "/bookstore/static/media/google.b88a7ce8.png"
+  },
+  {
+    "revision": "a00c795bf480ef1006cee7839901721c",
+    "url": "/bookstore/static/media/graphicTV@1x.a00c795b.png"
+  },
+  {
+    "revision": "4e5c8009d562e103764d1c3dd99abcac",
+    "url": "/bookstore/static/media/maskgroup2.4e5c8009.png"
+  },
+  {
+    "revision": "812e22ab8abfbca00775436ecff39d39",
+    "url": "/bookstore/static/media/menubar.812e22ab.png"
+  },
+  {
+    "revision": "90f5c2138bf66cb0d670629af75b62f9",
+    "url": "/bookstore/static/media/side_book_bg.90f5c213.png"
+  },
+  {
+    "revision": "7c07fa4976fbb4ac8e8cdf3e92413793",
+    "url": "/bookstore/static/media/tv.7c07fa49.png"
+  },
+  {
+    "revision": "0a5e1ff638562f580ff8899e27eb07ad",
+    "url": "/bookstore/static/media/tv_light.0a5e1ff6.png"
+  },
+  {
+    "revision": "b2e511e883ff34f14d0a6b093517d025",
+    "url": "/bookstore/static/media/yellowpath20.b2e511e8.png"
+  }
+]);
