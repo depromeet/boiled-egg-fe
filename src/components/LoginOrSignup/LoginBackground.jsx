2443c98095d1d346fe680883069431abe95725a3
eunbi <silver23rain@gmail.com>
2020-02-22 11:46:11 +0900

    [ui] 로그인 페이지 높이, 너비 100% 로 수정

--- src/components/LoginOrSignup/LoginBackground.jsx
+++ src/components/LoginOrSignup/LoginBackground.jsx
@@ -1,36 +1,34 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import styled from "styled-components";
 import graphicTV from "../../img/graphicTV@1x.png";
 
 class LoginBackground extends Component {
-
-    render() {
-        return (
-                <Background>
-                    <GraphicTV/>
-                   
-                </Background>   
-            );
-    }
+	render() {
+		return (
+			<Background>
+				<GraphicTV />
+			</Background>
+		);
+	}
 }
 
 const Background = styled.div`
-    background-color: #ffde2b;
-    width: 1920px;
-    height:1080px;
-    position: absolute;
-    top: 0px;
-    bottom: 0px;
-    z-index: 1;
+	background-color: #ffde2b;
+	width: 100%;
+	height: 100%;
+	position: absolute;
+	top: 0px;
+	bottom: 0px;
+	z-index: 1;
 `;
 const GraphicTV = styled.img.attrs({
-    src: graphicTV ,
-  })`
-    width:707.8px;
-    height:593.8px;
-    position: absolute;
-    top:125.5px;
-    left: 155px;
+	src: graphicTV
+})`
+	width: 707.8px;
+	height: 593.8px;
+	position: absolute;
+	top: 125.5px;
+	left: 155px;
 `;
 
 export default LoginBackground;
